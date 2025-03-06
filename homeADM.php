9124d0d2d22fc4a3e0bf8cf55f0551460d611693
Your Name <you@example.com>
2025-03-06 15:26:01 -0300

    06/02/2025

--- homeADM.php
+++ homeADM.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/css/styles.css">
-    <title>Resolve Aí </title>
+    <title>Resolve Aí - ADM</title>
     <!DOCTYPE html>
     <html lang="en">
 </head>
