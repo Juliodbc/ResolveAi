f968f65fe5cfa6260a08c8c333773f7075b9ae59
Your Name <you@example.com>
2025-03-07 09:38:27 -0300

    07/03 Co-authored-by: Rafael <Rafaelcstrauss@users.noreply.github.com> Co-authored-by: ceciquinho <ceciquinho@users.noreply.github.com>

---
+++ ajuda.php
@@ -0,0 +1,40 @@
+<?php
+include 'sidebar.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ajuda</title>
+</head>
+
+<body>
+    <div class="screen">
+
+    </div>
+    <div class="duvidas">
+
+    </div>
+    <div class="imagem">
+
+    </div>
+    <div class="">
+
+    </div>
+    <div class="screen">
+
+    </div>
+    <div class="screen">
+
+    </div>
+    <div class="screen">
+
+    </div>
+    <footer>
+
+    </footer>
+</body>
+
+</html>
